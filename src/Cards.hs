c69f4269879cb42ca19ce4bf7101079e481a6930
LucasBou <29388787+LucasBou@users.noreply.github.com>
2025-04-25 16:35:52 +0000

    added basic cards

---
+++ src/Cards.hs
@@ -0,0 +1,10 @@
+module Cards where
+
+data Color = Hearts | Spades | Diamonds | Clubs
+
+
+data Value = Num Int | Jack | Queen | King | Ace deriving (Ord, Eq)
+
+
+
+
--- test/Main.hs
+++ test/Main.hs
@@ -1,5 +1,6 @@
 module Main (main) where
 import Test.Hspec (Spec, describe, hspec, it, shouldBe)
+import Cards (Value(..))
 
 main :: IO ()
 main = hspec globalSpec
@@ -8,7 +9,10 @@ globalSpec :: Spec
 globalSpec = do
   describe "The Poskell tests for the library" $ do
     dummyTest
-
+    queenBetterThanJack
 
 dummyTest = it "ensures the test suite runs" $ do
   1 `shouldBe` 1
+
+queenBetterThanJack = it "ensures a queen beats a jack" $ do
+    Jack < Queen  `shouldBe` True
