5893b01978b18623bf274c10ae9343fcbab449f5
LucasBou <29388787+LucasBou@users.noreply.github.com>
2025-04-25 16:24:43 +0000

    added hspec as a test suite

--- test/Main.hs
+++ test/Main.hs
@@ -1,4 +1,14 @@
 module Main (main) where
+import Test.Hspec (Spec, describe, hspec, it, shouldBe)
 
 main :: IO ()
-main = putStrLn "Test suite not yet implemented."
+main = hspec globalSpec
+
+globalSpec :: Spec
+globalSpec = do
+  describe "The Poskell tests for the library" $ do
+    dummyTest
+
+
+dummyTest = it "ensures the test suite runs" $ do
+  1 `shouldBe` 1
