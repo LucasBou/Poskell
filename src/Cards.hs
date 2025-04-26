865bc275791e0c1bf26a62b0544c30cbb8f374c5
cracrou <delabourdonnayef@gmail.com>
2025-04-26 12:55:53 +0000

    add ord instance + tests

--- src/Cards.hs
+++ src/Cards.hs
@@ -7,7 +7,8 @@ data Value = Num Int | Jack | Queen | King | Ace deriving (Ord, Eq)
 
 data Card = Card { color :: Color, value :: Value} 
 instance Eq Card  where 
-    Card colora valuea == Card colorb valueb = valuea == valueb
+    Card _ valuea == Card _ valueb = valuea == valueb
     
 
-
+instance Ord Card where
+    Card _ valuea <= Card _ valueb = valuea<=valueb
--- test/Main.hs
+++ test/Main.hs
@@ -11,6 +11,8 @@ globalSpec = do
     dummyTest
     queenBetterThanJack
     queenDiamondEqualQueenHeart
+    aceHeartNotEqualToEightClubs
+    tenClubsBetterThanthreeHearts
 
 dummyTest = it "ensures the test suite runs" $ do
   1 `shouldBe` 1
@@ -20,3 +22,9 @@ queenBetterThanJack = it "ensures a queen beats a jack" $ do
 
 queenDiamondEqualQueenHeart =  it "ensures that a queen diamond is equal to queen heart" $ do
     Card Diamonds Queen == Card Hearts Queen `shouldBe` True
+
+aceHeartNotEqualToEightClubs = it "ensures that a ace heart is not equal to eight clubs" $ do
+    Card Hearts Ace /= Card Clubs  (Num 8) `shouldBe` True
+
+tenClubsBetterThanthreeHearts = it "ensures that 10 clubs us better than three hearts" $ do
+  Card Clubs (Num 10)> Card Hearts (Num 3) `shouldBe` True
