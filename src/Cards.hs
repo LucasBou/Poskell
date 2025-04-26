344b9d98b8e6e0f161a40dd7fc94765507a53bb5
cracrou <delabourdonnayef@gmail.com>
2025-04-26 07:42:41 +0000

    add card + eq relation + test

--- src/Cards.hs
+++ src/Cards.hs
@@ -5,6 +5,9 @@ data Color = Hearts | Spades | Diamonds | Clubs
 
 data Value = Num Int | Jack | Queen | King | Ace deriving (Ord, Eq)
 
-
+data Card = Card { color :: Color, value :: Value} 
+instance Eq Card  where 
+    Card colora valuea == Card colorb valueb = valuea == valueb
+    
 
 
--- test/Main.hs
+++ test/Main.hs
@@ -1,6 +1,6 @@
 module Main (main) where
 import Test.Hspec (Spec, describe, hspec, it, shouldBe)
-import Cards (Value(..))
+import Cards (Value(..), Color(..), Card(..))
 
 main :: IO ()
 main = hspec globalSpec
@@ -10,9 +10,13 @@ globalSpec = do
   describe "The Poskell tests for the library" $ do
     dummyTest
     queenBetterThanJack
+    queenDiamondEqualQueenHeart
 
 dummyTest = it "ensures the test suite runs" $ do
   1 `shouldBe` 1
 
 queenBetterThanJack = it "ensures a queen beats a jack" $ do
     Jack < Queen  `shouldBe` True
+
+queenDiamondEqualQueenHeart =  it "ensures that a queen diamond is equal to queen heart" $ do
+    Card Diamonds Queen == Card Hearts Queen `shouldBe` True
