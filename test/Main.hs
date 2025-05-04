f5eee7d867141f061212f705b60a817d27276827
Lucas Bouju <lucasbouju@gmail.com>
2025-05-04 19:34:49 +0000

    added the shouldBeGreaterThan Expectation

--- test/Main.hs
+++ test/Main.hs
@@ -13,12 +13,16 @@ globalSpec = do
     queenDiamondEqualQueenHeart
     aceHeartNotEqualToEightClubs
     tenClubsBetterThanthreeHearts
+    -- pairBetterThanHighCard 
+
+shouldBeGreaterThan :: (Ord a, Show a) => a -> a -> Expectation
+shouldBeGreaterThan a b = a `shouldSatisfy` (>b)
 
 dummyTest = it "ensures the test suite runs" $ do
   1 `shouldBe` 1
 
 queenBetterThanJack = it "ensures a queen beats a jack" $ do
-    Jack < Queen  `shouldBe` True
+    Queen `shouldBeGreaterThan` Jack
 
 queenDiamondEqualQueenHeart =  it "ensures that a queen diamond is equal to queen heart" $ do
     Card Diamonds Queen == Card Hearts Queen `shouldBe` True
@@ -27,4 +31,7 @@ aceHeartNotEqualToEightClubs = it "ensures that a ace heart is not equal to eigh
     Card Hearts Ace /= Card Clubs  (Num 8) `shouldBe` True
 
 tenClubsBetterThanthreeHearts = it "ensures that 10 clubs us better than three hearts" $ do
-  Card Clubs (Num 10)> Card Hearts (Num 3) `shouldBe` True
+  Card Clubs (Num 10) `shouldBeGreaterThan` Card Hearts (Num 3)
+
+pairBetterThanHighCard = it "ensures a pair wins over a high card" $ do
+  Pair (Jack) `shouldBeGreaterThan` High King
