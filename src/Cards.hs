6df82180fd310ab7e81797d899e78f167349bd60
Lucas Bouju <29388787+LucasBou@users.noreply.github.com>
2025-06-11 13:50:03 +0200

    added overloaded strings support for Card

--- src/Cards.hs
+++ src/Cards.hs
@@ -1,5 +1,7 @@
 module Cards where
 import Data.List
+import Data.String( IsString(..) )
+import Data.Maybe ( fromJust)
 data Color = Hearts | Spades | Diamonds | Clubs deriving (Eq,Ord,Show)
 
 
@@ -12,6 +14,33 @@ instance Eq Card  where
 
 instance Ord Card where
     Card _ valuea <= Card _ valueb = valuea<=valueb
+
+instance IsString Card where
+    fromString (firstChar : [secondChar]) = Card (fromJust $ colorFromChar secondChar) (fromJust $ valueFromChar firstChar)
+    fromString ('1' :'0' :[colorChar]) = Card (fromJust $ colorFromChar colorChar) Ten
+    fromString _ = undefined "unexpected string while constructing a Card"
+
+valueFromChar :: Char -> Maybe Value
+valueFromChar '2' = Just Two 
+valueFromChar '3' = Just Three 
+valueFromChar '4' = Just Four 
+valueFromChar '5' = Just Five 
+valueFromChar '6' = Just Six 
+valueFromChar '7' = Just Seven 
+valueFromChar '8' = Just Eight 
+valueFromChar '9' = Just Nine 
+valueFromChar 'J' = Just Jack 
+valueFromChar 'Q' = Just Queen 
+valueFromChar 'K' = Just King 
+valueFromChar 'A' = Just Ace 
+valueFromChar  _  = Nothing 
+
+colorFromChar :: Char -> Maybe Color
+colorFromChar '♥' = Just Hearts
+colorFromChar '♠' = Just Spades
+colorFromChar '♦' = Just Diamonds
+colorFromChar '♣' = Just Clubs
+colorFromChar  _  = Nothing
 
 type Cards = [Card]
 
--- test/Main.hs
+++ test/Main.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -Wno-missing-signatures #-}
 module Main (main) where
 import Test.Hspec (Spec, describe, hspec, it, shouldBe,shouldSatisfy, Expectation)
@@ -58,68 +59,68 @@ aceHeartNotEqualToEightClubs = it "ensures that a ace heart is not equal to eigh
 tenClubsBetterThanthreeHearts = it "ensures that 10 clubs us better than three hearts" $ do
   Card Clubs Ten> Card Hearts Three `shouldBe` True
 
-detectFlush = it "ensures that 2, 3, 7,8, and 10 of spades is a flush" $ do
-  isFlush [Card Spades Two, Card Spades Three, Card Spades Seven, Card Spades Eight, Card Spades Ten] `shouldBe` True
+detectFlush = it "ensures that 2, 3, 7, 8, and 10 of spades is a flush" $ do
+  isFlush ["2♠","3♠","7♠","8♠","10♠"] `shouldBe` True
 
-detectNotFlush1 = it "ensures that 2, 3, 7,8 of spades + Jack of heart is not a flush" $ do
-  isFlush [Card Spades Two, Card Spades Three, Card Spades Seven, Card Spades Eight, Card Hearts Jack] `shouldBe` False
+detectNotFlush1 = it "ensures that 2, 3, 7, 8 of spades + Jack of heart is not a flush" $ do
+  isFlush ["2♠","3♠","7♠","8♠","J♥"] `shouldBe` False
 
-detectNotFlush2 = it "ensures that 2 of heart + ace of spades + king of clubs + queen of diamons + Jack of heart is not a flush" $ do
-  isFlush [Card Hearts Two, Card Spades Ace, Card Clubs King, Card Diamonds Queen, Card Hearts Jack] `shouldBe` False
+detectNotFlush2 = it "ensures that 2 of heart + ace of spades + king of clubs + queen of diamonds + Jack of heart is not a flush" $ do
+  isFlush ["2♥","A♠","K♣","Q♦","J♥"] `shouldBe` False
 
 detectStraight1 = it "ensures that 2 of heart, 3 of spades, 4 of diamonds, 5 of clubs and 6 of spades is a straight" $ do
-  isStraight [Card Hearts Two, Card Spades Three, Card Diamonds Four, Card Clubs Five, Card Spades Six] `shouldBe` True
+  isStraight ["2♥","3♠","4♦","5♣","6♠"] `shouldBe` True
 
 detectStraight2 = it "ensures that 10 of heart, jack of spades, queen of diamonds, king of clubs and ace of spades is a straight" $ do
-  isStraight [Card Hearts Ten, Card Spades Jack, Card Diamonds Queen, Card Clubs King, Card Spades Ace] `shouldBe` True
+  isStraight ["10♥","J♠","Q♦","K♣","A♠"] `shouldBe` True
 
 detectNotStraight1 = it "ensures that 2 of heart, 3 of spades, 4 of diamonds, Jack of clubs and 6 of spades is a straight" $ do
-  isStraight [Card Hearts Two, Card Spades Three, Card Diamonds Four, Card Clubs Jack, Card Spades Six] `shouldBe` False
+  isStraight ["2♥","3♠","4♦","J♣","6♠"] `shouldBe` False
 
 detectNotStraight2 = it "ensures that 10 of heart, jack of spades, queen of diamonds, king of clubs and two of spades is a straight" $ do
-  isStraight [Card Hearts Ten, Card Spades Jack, Card Diamonds Queen, Card Clubs King, Card Spades Two] `shouldBe` False
+  isStraight ["10♥","J♠","Q♦","K♣","2♠"] `shouldBe` False
 
 detectFlushStraight = it "ensures that 2, 3, 4,5, and 6 of diamonds is a straight flush" $ do
-  isFlush [Card Diamonds Two, Card Diamonds Three, Card Diamonds Four, Card Diamonds Five, Card Diamonds Six] `shouldBe` True
+  isFlush ["2♦","3♦","4♦","5♦","6♦"] `shouldBe` True
 
 detectNotFlushStraight = it "ensures that 2, 3, 4,5 of diamonds + 6 of Spades is not a straight flush" $ do
-  isFlush [Card Diamonds Two, Card Diamonds Three, Card Diamonds Four, Card Diamonds Five, Card Spades Six] `shouldBe` False
+  isFlush ["2♦","3♦","4♦","5♦","6♠"] `shouldBe` False
 
 detectFourOfAKind = it "ensures that 2 of hearts, diamons, spades, clubs + 3 of spades is four of a kind" $ do
-  isFourOfAKind [Card Diamonds Two, Card Spades Two, Card Clubs Two, Card Hearts Two, Card Spades Three] `shouldBe` True
+  isFourOfAKind ["2♦","2♠","2♣","2♥","3♠"] `shouldBe` True
 
 detectNotFourOfAKind = it "ensures that 2 of hearts, diamons, spades, + 5 of clubs + 3 of spades is four of a kind" $ do
-  isFourOfAKind [Card Diamonds Two, Card Spades Two, Card Clubs Two, Card Clubs Five, Card Spades Three] `shouldBe` False
+  isFourOfAKind ["2♦","2♠","2♣","5♣","3♠"] `shouldBe` False
 
 detectNotThreeOfAKind = it "ensures that 2 of hearts, diamons, Jack of spades, queen of clubs + 3 of spades is not three of a kind" $ do
-  isThreeOfAKind [Card Diamonds Two, Card Spades Jack, Card Clubs Queen, Card Hearts Two, Card Spades Three] `shouldBe` False
+  isThreeOfAKind ["2♦","J♠","Q♣","2♥","3♠"] `shouldBe` False
 
 detectThreeOfAKind = it "ensures that 2 of hearts, diamons, spades, + 5 of clubs + 3 of spades is three of a kind" $ do
-  isThreeOfAKind [Card Diamonds Two, Card Spades Two, Card Clubs Two, Card Clubs Five, Card Spades Three] `shouldBe` True
+  isThreeOfAKind ["2♦","2♠","2♣","5♣","3♠"] `shouldBe` True
 
 detectPair = it "ensures that Jack of spades and hearts + 3 of clubs + king Diamonds + Ace of spades" $ do
-  isPair [Card Spades Jack, Card Hearts Jack, Card Clubs Three, Card Diamonds King, Card Spades Ace] `shouldBe` True
+  isPair ["J♠","J♥","3♣","K♦","A♠"] `shouldBe` True
 
 detectNotPair= it "ensures that Jack of spades + seven of spades, hearts + 3 of clubs + king Diamonds + Ace of spades" $ do
-  isPair [Card Spades Jack, Card Spades Seven, Card Clubs Three, Card Diamonds King, Card Spades Ace] `shouldBe` False
+  isPair ["J♠","7♠","3♣","K♦","A♠"] `shouldBe` False
 
 detectTwoPairs = it "ensures that Jack of spades and hearts + 3 of clubs and diamonds + Ace of spades" $ do
-  isTwoPairs [Card Spades Jack, Card Hearts Jack, Card Clubs Three, Card Diamonds Three, Card Spades Ace] `shouldBe` True
+  isTwoPairs ["J♠","J♥","3♣","3♦","A♠"] `shouldBe` True
 
 detectNotTwoPairs =it "ensures that Jack of spades and hearts + 3 of clubs + king Diamonds + Ace of spades" $ do
-  isTwoPairs [Card Spades Jack, Card Hearts Jack, Card Clubs Three, Card Diamonds King, Card Spades Ace] `shouldBe` False
+  isTwoPairs ["J♠","J♥","3♣","K♦","A♠"] `shouldBe` False
 
 detectFull= it "ensures that Jack of spades and hearts and diamonds + 3 of clubs and diamonds " $ do
-  isFull [Card Spades Jack, Card Hearts Jack, Card Diamonds Jack, Card Diamonds Three, Card Clubs Three] `shouldBe` True
+  isFull ["J♠","J♥","J♦","3♦","3♣"] `shouldBe` True
 
 detectNotFull = it "ensures that Jack of spades and hearts and diamonds + 3 of diamonds + 2 of clubs " $ do
-  isFull [Card Spades Jack, Card Hearts Jack, Card Diamonds Jack, Card Diamonds Three, Card Clubs Two] `shouldBe` False
+  isFull ["J♠","J♥","J♦","3♦","2♣"] `shouldBe` False
 
 detectStraightWithHoles = it "ensures that quads of 2s and a six is not a straight" $ do
-  isStraight [Card Spades Two, Card Hearts Two, Card Diamonds Two, Card Clubs Two, Card Clubs Six] `shouldBe` False
+  isStraight ["2♠","2♥","2♦","2♣","6♣"] `shouldBe` False
+
 detectLowestStraight = it "ensures that a series of Ace to 4 is a straight" $ do
-  isStraight [Card Spades Ace, Card Hearts Two, Card Diamonds Three, Card Clubs Four, Card Clubs Five] `shouldBe` True
-  Card Clubs Ten `shouldBeGreaterThan` Card Hearts Three
+  isStraight ["A♠","2♥","3♦","4♣","5♣"] `shouldBe` True
 
 pairBetterThanHighCard = it "ensures a pair wins over a high card" $ do
   Pair Jack noKicker `shouldBeGreaterThan` High King noKicker
