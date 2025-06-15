0d8f63c1f14df43b2df6a434629b53888cd40ed9
Lucas Bouju <29388787+LucasBou@users.noreply.github.com>
2025-06-15 22:30:48 +0200

    ran ormolu code formatter

--- src/Cards.hs
+++ src/Cards.hs
@@ -1,43 +1,43 @@
 module Cards where
-import Data.String( IsString(..) )
-import Data.Maybe ( fromJust)
 
-data Color = Hearts | Spades | Diamonds | Clubs deriving (Eq,Ord,Show)
+import Data.Maybe (fromJust)
+import Data.String (IsString (..))
 
-data Value = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten| Jack | Queen | King | Ace deriving (Ord, Eq,Show, Enum)
+data Color = Hearts | Spades | Diamonds | Clubs deriving (Eq, Ord, Show)
 
-data Card = Card { color :: Color, value :: Value} deriving (Show)
+data Value = Two | Three | Four | Five | Six | Seven | Eight | Nine | Ten | Jack | Queen | King | Ace deriving (Ord, Eq, Show, Enum)
 
-instance Eq Card  where 
-    Card _ valuea == Card _ valueb = valuea == valueb
+data Card = Card {color :: Color, value :: Value} deriving (Show)
+
+instance Eq Card where
+  Card _ valuea == Card _ valueb = valuea == valueb
 
 instance Ord Card where
-    Card _ valuea <= Card _ valueb = valuea<=valueb
+  Card _ valuea <= Card _ valueb = valuea <= valueb
 
 instance IsString Card where
-    fromString (firstChar : [secondChar]) = Card (fromJust $ colorFromChar secondChar) (fromJust $ valueFromChar firstChar)
-    fromString ('1' :'0' :[colorChar]) = Card (fromJust $ colorFromChar colorChar) Ten
-    fromString _ = undefined "unexpected string while constructing a Card"
+  fromString (firstChar : [secondChar]) = Card (fromJust $ colorFromChar secondChar) (fromJust $ valueFromChar firstChar)
+  fromString ('1' : '0' : [colorChar]) = Card (fromJust $ colorFromChar colorChar) Ten
+  fromString _ = undefined "unexpected string while constructing a Card"
 
 valueFromChar :: Char -> Maybe Value
-valueFromChar '2' = Just Two 
-valueFromChar '3' = Just Three 
-valueFromChar '4' = Just Four 
-valueFromChar '5' = Just Five 
-valueFromChar '6' = Just Six 
-valueFromChar '7' = Just Seven 
-valueFromChar '8' = Just Eight 
-valueFromChar '9' = Just Nine 
-valueFromChar 'J' = Just Jack 
-valueFromChar 'Q' = Just Queen 
-valueFromChar 'K' = Just King 
-valueFromChar 'A' = Just Ace 
-valueFromChar  _  = Nothing 
+valueFromChar '2' = Just Two
+valueFromChar '3' = Just Three
+valueFromChar '4' = Just Four
+valueFromChar '5' = Just Five
+valueFromChar '6' = Just Six
+valueFromChar '7' = Just Seven
+valueFromChar '8' = Just Eight
+valueFromChar '9' = Just Nine
+valueFromChar 'J' = Just Jack
+valueFromChar 'Q' = Just Queen
+valueFromChar 'K' = Just King
+valueFromChar 'A' = Just Ace
+valueFromChar _ = Nothing
 
 colorFromChar :: Char -> Maybe Color
 colorFromChar '♥' = Just Hearts
 colorFromChar '♠' = Just Spades
 colorFromChar '♦' = Just Diamonds
 colorFromChar '♣' = Just Clubs
-colorFromChar  _  = Nothing
-
+colorFromChar _ = Nothing
--- test/Main.hs
+++ test/Main.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -Wno-missing-signatures #-}
+
 module Main (main) where
-import Test.Hspec (Spec, describe, hspec, it, shouldBe,shouldSatisfy, Expectation)
-import Cards (Value(..), Color(..), Card(..))
+
+import Cards (Card (..), Color (..), Value (..))
 import Hands
+import Test.Hspec (Expectation, Spec, describe, hspec, it, shouldBe, shouldSatisfy)
 
 main :: IO ()
 main = hspec globalSpec
@@ -35,92 +37,92 @@ globalSpec = do
     detectNotTwoPairs
     detectFull
     detectNotFull
-    detectStraightWithHoles 
-    detectLowestStraight 
-    pairBetterThanHighCard 
-    pairOfKingsBetterThanPairOfQueens 
-    samePairShouldBeDifferentiatedWithkickers 
+    detectStraightWithHoles
+    detectLowestStraight
+    pairBetterThanHighCard
+    pairOfKingsBetterThanPairOfQueens
+    samePairShouldBeDifferentiatedWithkickers
 
 shouldBeGreaterThan :: (Ord a, Show a) => a -> a -> Expectation
-shouldBeGreaterThan a b = a `shouldSatisfy` (>b)
+shouldBeGreaterThan a b = a `shouldSatisfy` (> b)
 
 dummyTest = it "ensures the test suite runs" $ do
   True `shouldBe` True
 
 queenBetterThanJack = it "ensures a queen beats a jack" $ do
-    Queen `shouldBeGreaterThan` Jack
+  Queen `shouldBeGreaterThan` Jack
 
-queenDiamondEqualQueenHeart =  it "ensures that a queen diamond is equal to queen heart" $ do
-    Card Diamonds Queen == Card Hearts Queen `shouldBe` True
+queenDiamondEqualQueenHeart = it "ensures that a queen diamond is equal to queen heart" $ do
+  Card Diamonds Queen == Card Hearts Queen `shouldBe` True
 
 aceHeartNotEqualToEightClubs = it "ensures that a ace heart is not equal to eight clubs" $ do
-    Card Hearts Ace /= Card Clubs  Eight `shouldBe` True
+  Card Hearts Ace /= Card Clubs Eight `shouldBe` True
 
 tenClubsBetterThanthreeHearts = it "ensures that 10 clubs us better than three hearts" $ do
-  Card Clubs Ten> Card Hearts Three `shouldBe` True
+  Card Clubs Ten > Card Hearts Three `shouldBe` True
 
 detectFlush = it "ensures that 2, 3, 7, 8, and 10 of spades is a flush" $ do
-  isFlush ["2♠","3♠","7♠","8♠","10♠"] `shouldBe` True
+  isFlush ["2♠", "3♠", "7♠", "8♠", "10♠"] `shouldBe` True
 
 detectNotFlush1 = it "ensures that 2, 3, 7, 8 of spades + Jack of heart is not a flush" $ do
-  isFlush ["2♠","3♠","7♠","8♠","J♥"] `shouldBe` False
+  isFlush ["2♠", "3♠", "7♠", "8♠", "J♥"] `shouldBe` False
 
 detectNotFlush2 = it "ensures that 2 of heart + ace of spades + king of clubs + queen of diamonds + Jack of heart is not a flush" $ do
-  isFlush ["2♥","A♠","K♣","Q♦","J♥"] `shouldBe` False
+  isFlush ["2♥", "A♠", "K♣", "Q♦", "J♥"] `shouldBe` False
 
 detectStraight1 = it "ensures that 2 of heart, 3 of spades, 4 of diamonds, 5 of clubs and 6 of spades is a straight" $ do
-  isStraight ["2♥","3♠","4♦","5♣","6♠"] `shouldBe` True
+  isStraight ["2♥", "3♠", "4♦", "5♣", "6♠"] `shouldBe` True
 
 detectStraight2 = it "ensures that 10 of heart, jack of spades, queen of diamonds, king of clubs and ace of spades is a straight" $ do
-  isStraight ["10♥","J♠","Q♦","K♣","A♠"] `shouldBe` True
+  isStraight ["10♥", "J♠", "Q♦", "K♣", "A♠"] `shouldBe` True
 
 detectNotStraight1 = it "ensures that 2 of heart, 3 of spades, 4 of diamonds, Jack of clubs and 6 of spades is a straight" $ do
-  isStraight ["2♥","3♠","4♦","J♣","6♠"] `shouldBe` False
+  isStraight ["2♥", "3♠", "4♦", "J♣", "6♠"] `shouldBe` False
 
 detectNotStraight2 = it "ensures that 10 of heart, jack of spades, queen of diamonds, king of clubs and two of spades is a straight" $ do
-  isStraight ["10♥","J♠","Q♦","K♣","2♠"] `shouldBe` False
+  isStraight ["10♥", "J♠", "Q♦", "K♣", "2♠"] `shouldBe` False
 
 detectFlushStraight = it "ensures that 2, 3, 4,5, and 6 of diamonds is a straight flush" $ do
-  isFlush ["2♦","3♦","4♦","5♦","6♦"] `shouldBe` True
+  isFlush ["2♦", "3♦", "4♦", "5♦", "6♦"] `shouldBe` True
 
 detectNotFlushStraight = it "ensures that 2, 3, 4,5 of diamonds + 6 of Spades is not a straight flush" $ do
-  isFlush ["2♦","3♦","4♦","5♦","6♠"] `shouldBe` False
+  isFlush ["2♦", "3♦", "4♦", "5♦", "6♠"] `shouldBe` False
 
 detectFourOfAKind = it "ensures that 2 of hearts, diamons, spades, clubs + 3 of spades is four of a kind" $ do
-  isFourOfAKind ["2♦","2♠","2♣","2♥","3♠"] `shouldBe` True
+  isFourOfAKind ["2♦", "2♠", "2♣", "2♥", "3♠"] `shouldBe` True
 
 detectNotFourOfAKind = it "ensures that 2 of hearts, diamons, spades, + 5 of clubs + 3 of spades is four of a kind" $ do
-  isFourOfAKind ["2♦","2♠","2♣","5♣","3♠"] `shouldBe` False
+  isFourOfAKind ["2♦", "2♠", "2♣", "5♣", "3♠"] `shouldBe` False
 
 detectNotThreeOfAKind = it "ensures that 2 of hearts, diamons, Jack of spades, queen of clubs + 3 of spades is not three of a kind" $ do
-  isThreeOfAKind ["2♦","J♠","Q♣","2♥","3♠"] `shouldBe` False
+  isThreeOfAKind ["2♦", "J♠", "Q♣", "2♥", "3♠"] `shouldBe` False
 
 detectThreeOfAKind = it "ensures that 2 of hearts, diamons, spades, + 5 of clubs + 3 of spades is three of a kind" $ do
-  isThreeOfAKind ["2♦","2♠","2♣","5♣","3♠"] `shouldBe` True
+  isThreeOfAKind ["2♦", "2♠", "2♣", "5♣", "3♠"] `shouldBe` True
 
 detectPair = it "ensures that Jack of spades and hearts + 3 of clubs + king Diamonds + Ace of spades" $ do
-  isPair ["J♠","J♥","3♣","K♦","A♠"] `shouldBe` True
+  isPair ["J♠", "J♥", "3♣", "K♦", "A♠"] `shouldBe` True
 
-detectNotPair= it "ensures that Jack of spades + seven of spades, hearts + 3 of clubs + king Diamonds + Ace of spades" $ do
-  isPair ["J♠","7♠","3♣","K♦","A♠"] `shouldBe` False
+detectNotPair = it "ensures that Jack of spades + seven of spades, hearts + 3 of clubs + king Diamonds + Ace of spades" $ do
+  isPair ["J♠", "7♠", "3♣", "K♦", "A♠"] `shouldBe` False
 
 detectTwoPairs = it "ensures that Jack of spades and hearts + 3 of clubs and diamonds + Ace of spades" $ do
-  isTwoPairs ["J♠","J♥","3♣","3♦","A♠"] `shouldBe` True
+  isTwoPairs ["J♠", "J♥", "3♣", "3♦", "A♠"] `shouldBe` True
 
-detectNotTwoPairs =it "ensures that Jack of spades and hearts + 3 of clubs + king Diamonds + Ace of spades" $ do
-  isTwoPairs ["J♠","J♥","3♣","K♦","A♠"] `shouldBe` False
+detectNotTwoPairs = it "ensures that Jack of spades and hearts + 3 of clubs + king Diamonds + Ace of spades" $ do
+  isTwoPairs ["J♠", "J♥", "3♣", "K♦", "A♠"] `shouldBe` False
 
-detectFull= it "ensures that Jack of spades and hearts and diamonds + 3 of clubs and diamonds " $ do
-  isFull ["J♠","J♥","J♦","3♦","3♣"] `shouldBe` True
+detectFull = it "ensures that Jack of spades and hearts and diamonds + 3 of clubs and diamonds " $ do
+  isFull ["J♠", "J♥", "J♦", "3♦", "3♣"] `shouldBe` True
 
 detectNotFull = it "ensures that Jack of spades and hearts and diamonds + 3 of diamonds + 2 of clubs " $ do
-  isFull ["J♠","J♥","J♦","3♦","2♣"] `shouldBe` False
+  isFull ["J♠", "J♥", "J♦", "3♦", "2♣"] `shouldBe` False
 
 detectStraightWithHoles = it "ensures that quads of 2s and a six is not a straight" $ do
-  isStraight ["2♠","2♥","2♦","2♣","6♣"] `shouldBe` False
+  isStraight ["2♠", "2♥", "2♦", "2♣", "6♣"] `shouldBe` False
 
 detectLowestStraight = it "ensures that a series of Ace to 4 is a straight" $ do
-  isStraight ["A♠","2♥","3♦","4♣","5♣"] `shouldBe` True
+  isStraight ["A♠", "2♥", "3♦", "4♣", "5♣"] `shouldBe` True
 
 pairBetterThanHighCard = it "ensures a pair wins over a high card" $ do
   Pair Jack noKicker `shouldBeGreaterThan` High King noKicker
